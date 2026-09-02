c6cc851627762a5938a5ba6f8a9edccb7128a1b8
leolindqvist123 <leolikro01@gmail.com>
2026-09-02 11:57:37 +0200

    add recipe data models

--- backend/data_models.py
+++ backend/data_models.py
@@ -1,13 +1,26 @@
 from pydantic import BaseModel
 from typing import Literal
 
-class Ingredients(BaseModel):
+class Ingredient(BaseModel):
     name: str
     calories: int
     protein_g: int
-    carbohydrates_g: float
+    carbohydrates_g: int
 
-    category: Literal["fruit", "vegetable", "meat", "fish", "dairy", "bakery", "snacks", "vegan"]
+    category: Literal["fruit", "vegetable", "meat", "fish", "dairy", "bakery", "snacks", "other"]
 
 class ScanResponse(BaseModel):
-    ingredients: list[Ingredients]
+    ingredients: list[Ingredient]
+
+
+class Recipe(BaseModel):
+    name: str
+    instructions: list[str]
+    ingredients_missing: list[str]
+    ingredients_available: list[str]
+    calories: int
+    protein_g: int
+    carbohydrates_g: int
+
+class RecipeResponse(BaseModel):
+    recipes: list[Recipe]
